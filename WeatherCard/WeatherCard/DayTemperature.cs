647486c4a80188c0a5b1468635f594fb88bb5cb2
Toleutai Bekzat <toleutaib@gmail.com>
2019-12-02 19:10:13 +0600

    Сделал показатель погоды

---
+++ WeatherCard/WeatherCard/DayTemperature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherCard
+{
+	public class DayTemperature
+	{
+		public string Day { get; set; }
+		public double Value { get; set; }
+		public double Wind { get; set; }
+		public string Symbol { get; set; }
+		public int Pressure { get; set; }
+	}
+}
---
+++ WeatherCard/WeatherCard/Frequency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherCard
+{
+	class Frequency
+	{
+		public string Symbol { get; set; }
+		public int Value { get; set; }
+	}
+}
